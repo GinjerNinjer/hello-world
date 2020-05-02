f67fd8ae51eafd3c9bfbeee180d08d79ccab5d6b
GinjerNinjer <64621939+GinjerNinjer@users.noreply.github.com>
2020-05-02 11:24:53 +0100

    Add files via upload
    This is the java file

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+/**
+*	The HelloWorld App implements the class helloworld
+*	which will print Hello World! 
+*  	Its my first ever bit of code, I hope my commenting stays this good!
+*/
+class helloworld{
+	public static void main(String[] args){
+		System.out.println("Hello World!"); //Displays the string Hello World!
+	}
+}
